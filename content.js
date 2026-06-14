e0ff3c21ca12f2f0b86f796df1e379c01c3c0866
Gaspar <gasparlarrainv@gmail.com>
2026-06-13 21:24:37 -0400

    Update design

--- content.js
+++ content.js
@@ -5,6 +5,6 @@ fetch(chrome.runtime.getURL("GoogleItem.html"))
   .then(html => {
     const populatedHtml = html.replaceAll("__SEARCH_QUERY__", encodeURIComponent(searchQuery));
     const TopStuff = document.querySelector("#topstuff")
-    TopStuff.style = "background-color: #139A43; border-radius: 20px; padding: 20px; border: 2px solid black; margin-bottom: 20px;";
+    TopStuff.style = "background-color: #87FF65; border-radius: 20px; padding: 20px; border: 2px solid black; margin-bottom: 20px;";
     TopStuff.innerHTML = populatedHtml;
   });
